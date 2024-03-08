d2bf678c6863128496f30f6c0375de3e2424b786
gonzalo00913 <gonzalo-masa@outlook.com>
2024-03-08 10:22:34 -0300

    style

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,4 +1,5 @@
 import style from "../Projects/projects.module.css";
+import github from "../../assets/github.png";
 
 const Projects = () => {
   return (
@@ -6,9 +7,17 @@ const Projects = () => {
       <h2 className={style.titleProjects}>Proyectos</h2>
       <section className={style.containerProyects}>
         <div className={style.projects}>
-          <a href="https://granodeoro.vercel.app/" target="_blanck">
-            <h4>Grano de Oro</h4>
-          </a>
+          <div className={style.titleGh}>
+            <a href="https://granodeoro.vercel.app/" target="_blanck">
+              <h4>Grano de Oro</h4>
+            </a>
+            <a
+              href="https://github.com/gonzalo00913/Dashboard"
+              target="_blanck"
+            >
+              <img className={style.iconsMediaSize} src={github} alt="GitHub" />
+            </a>
+          </div>
           <p className={style.desProjects}>
             Contribuí al desarrollo front-end de una pagina web dedicada a la
             venta de café.
@@ -24,12 +33,20 @@ const Projects = () => {
           </div>
         </div>
         <div className={style.projects}>
-          <a href="https://meddyplus.netlify.app/" target="_blanck">
-            <h4>Meddy plus</h4>
-          </a>
+          <div className={style.titleGh}>
+            <a href="https://meddyplus.netlify.app/" target="_blanck">
+              <h4>Meddy plus</h4>
+            </a>
+            <a
+              href="https://github.com/gonzalo00913/MeddyPlus-Gestion-de-Historiales-Medicos"
+              target="_blanck"
+            >
+              <img className={style.iconsMediaSize} src={github} alt="GitHub" />
+            </a>
+          </div>
           <p className={style.desProjects}>
-            Contribuí al desarrollo back-end de una pagina
-            web dedicada a la gestión de historiales clínicos.
+            Contribuí al desarrollo back-end de una pagina web dedicada a la
+            gestión de historiales clínicos.
           </p>
           <div className={style.containerTecno}>
             <span className={style.spanTecno}>TypeScript</span>
@@ -40,11 +57,21 @@ const Projects = () => {
           </div>
         </div>
         <div className={style.projects}>
-          <a href="https://food-app-gonzalo-masa.netlify.app/" target="_blanck">
-          <h4>Food App</h4>
-          </a>
+          <div className={style.titleGh}>
+            <a
+              href="https://food-app-gonzalo-masa.netlify.app/"
+              target="_blanck"
+            >
+              <h4>Food App</h4>
+            </a>
+            <a
+              href="https://github.com/gonzalo00913/Homemade-delights-Recetas-de-comida"
+              target="_blanck"
+            >
+              <img className={style.iconsMediaSize} src={github} alt="GitHub" />
+            </a>
+          </div>
           <p className={style.desProjects}>
-            {" "}
             Permite buscar, filtrar y crear recetas, y utiliza Redux para la
             gestión del estado de la aplicación
           </p>
@@ -59,9 +86,20 @@ const Projects = () => {
           </div>
         </div>
         <div className={style.projects}>
-        <a href="https://gonzalo00913.github.io/Project--Three-Project-Calculator/" target="_blanck">
-          <h4>Calculator</h4>
-          </a>
+          <div className={style.titleGh}>
+            <a
+              href="https://gonzalo00913.github.io/Project--Three-Project-Calculator/"
+              target="_blanck"
+            >
+              <h4>Calculator</h4>
+            </a>
+            <a
+              href="https://github.com/gonzalo00913/Project--Three-Project-Calculator"
+              target="_blanck"
+            >
+              <img className={style.iconsMediaSize} src={github} alt="GitHub" />
+            </a>
+          </div>
           <p className={style.desProjects}>
             Utilicé JavaScript para manipular el DOM y crear la lógica de la
             calculadora.
@@ -73,9 +111,20 @@ const Projects = () => {
           </div>
         </div>
         <div className={style.projects}>
-        <a href="https://gonzalo00913.github.io/Project-Rock-Paper-Scissors/" target="_blanck">
-          <h4>Rock-Paper-Scissors</h4>
-          </a>
+          <div className={style.titleGh}>
+            <a
+              href="https://gonzalo00913.github.io/Project-Rock-Paper-Scissors/"
+              target="_blanck"
+            >
+              <h4>Rock-Paper-Scissors</h4>
+            </a>
+            <a
+              href="https://github.com/gonzalo00913/Project-Rock-Paper-Scissors"
+              target="_blanck"
+            >
+              <img className={style.iconsMediaSize} src={github} alt="GitHub" />
+            </a>
+          </div>
           <p className={style.desProjects}>
             En mis primeros pasos, realicé un proyecto utilizando JavaScript
             Vanilla para manipular el DOM.
@@ -87,12 +136,23 @@ const Projects = () => {
           </div>
         </div>
         <div className={style.projects}>
-        <a href="https://gonzalo00913.github.io/Etch-A-Sketch/" target="_blanck">
-          <h4>Etch-A-Sketch</h4>
-          </a>
+          <div className={style.titleGh}>
+            <a
+              href="https://gonzalo00913.github.io/Etch-A-Sketch/"
+              target="_blanck"
+            >
+              <h4>Etch-A-Sketch</h4>
+            </a>
+            <a
+              href="https://github.com/gonzalo00913/Etch-A-Sketch"
+              target="_blanck"
+            >
+              <img className={style.iconsMediaSize} src={github} alt="GitHub" />
+            </a>
+          </div>
           <p className={style.desProjects}>
-            Implementé eventos del mouse para capturar los
-            movimientos del cursor y dibujar líneas en el lienzo
+            Implementé eventos del mouse para capturar los movimientos del
+            cursor y dibujar líneas en el lienzo
           </p>
           <div className={style.containerTecno}>
             <span className={style.spanTecno}>JavaScript</span>
