efc378b9e7c3602fe79c555380fc6b18f4df88e9
gonzalo00913 <gonzalo-masa@outlook.com>
2024-08-07 08:46:24 -0300

    nuevo diseño en proyectos

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -7,35 +7,34 @@ const Nav = () => {
   return (
     <div>
       <nav className={style.navbar}>
-       
         <NavLink to="/">
-         <div className="">
-          <img className={style.imgLogo} src={imgLogo} alt="logo" />
-        </div>
+          <div className="">
+            <img className={style.imgLogo} src={imgLogo} alt="logo" />
+          </div>
         </NavLink>
-      
+
         <div className={style.containerLinks}>
           <div>
-          <NavLink className={style.link} to="/" exact title="Home">
-           <i className="fa-solid fa-house"></i>
-          <span className={style.nameIcon}>Inicio</span>
-          </NavLink>
+            <NavLink className={style.link} to="/" exact title="Home">
+              <i className="fa-solid fa-house"></i>
+              <span className={style.nameIcon}>Inicio</span>
+            </NavLink>
           </div>
-          <NavLink className={style.link} to="/Skills" title="Skills">
-              <i className="fa-solid fa-gear"></i>
-          <span className={style.nameIcon}>Tecnologías</span>
-          </NavLink>
 
           <NavLink className={style.link} to="/Projects" title="Projects">
-               <i className="fa-solid fa-sheet-plastic"></i>
-          <span className={style.nameIcon}>Proyectos</span>
+            <i className="fa-solid fa-sheet-plastic"></i>
+            <span className={style.nameIcon}>Proyectos</span>
+          </NavLink>
+
+          <NavLink className={style.link} to="/Skills" title="Skills">
+            <i className="fa-solid fa-gear"></i>
+            <span className={style.nameIcon}>Tecnologías</span>
           </NavLink>
 
           <NavLink className={style.link} to="/Contact">
-           <i className="fa-solid fa-phone"></i>
-          <span className={style.nameIcon}>Contacto</span>
+            <i className="fa-solid fa-phone"></i>
+            <span className={style.nameIcon}>Contacto</span>
           </NavLink>
-
         </div>
       </nav>
     </div>
--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,190 +1,111 @@
+import React, { useState } from 'react';
+import Modal from 'react-modal';
 import style from "../Projects/projects.module.css";
 import github from "../../assets/github.png";
+import automenu from "../../assets/Projects/Video21.gif";
+import granoDeOro from "../../assets/Projects/grano-de-oro2.gif";
+import meddyPlus from "../../assets/Projects/meddyplus.gif";
+
+Modal.setAppElement('#root');
 
 const Projects = () => {
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+  const [selectedImage, setSelectedImage] = useState(null);
+
+  const openModal = (image) => {
+    setSelectedImage(image);
+    setModalIsOpen(true);
+  };
+
+  const closeModal = () => {
+    setModalIsOpen(false);
+    setSelectedImage(null);
+  };
+
   return (
     <div className={style.containerAll}>
       <h2 className={style.titleProjects}>Proyectos</h2>
       <section className={style.containerProyects}>
-      <div className={style.projects}>
-          <div className={style.titleGh}>
-            <a href="https://auto-menu-app.vercel.app/admin/login" target="_blanck">
-              <h4>Auto Menu</h4>
-            </a>
+        <div>
+          <div className={style.card}>
+            <div className={style.cardimage}>
+              <img className={style.imgsize} src={automenu} onClick={() => openModal(automenu)} />
+            </div>
             <a
               href="https://github.com/nicoflorentin/auto-menu/tree/main"
-              target="_blanck"
+              target="_blank"
             >
-              <img className={style.iconsMediaSize} src={github} alt="GitHub" />
+              <h4 className={style.cardtitle}>Auto Menu</h4>
             </a>
-          </div>
-          <p className={style.desProjects}>
-          Permite crear fácilmente un diseño de menú para un restaurante o bar sin necesidad de diseñar.
-          </p>
-          <div className={style.containerTecno}>
-            <span className={style.spanTecno}>JavaScript</span>
-            <span className={style.spanTecno}>Vite</span>
-            <span className={style.spanTecno}>Redux</span>
-            <span className={style.spanTecno}>Express</span>
-            <span className={style.spanTecno}>Node</span>
-            <span className={style.spanTecno}>MongoDB</span>
+            <p className={style.cardbody}>
+              Permite crear fácilmente un diseño de menú para un restaurante o
+              bar sin necesidad de diseñar.
+            </p>
+            <div className={style.containerTecno}>
+              <span className={style.spanTecno}>JavaScript</span>
+              <span className={style.spanTecno}>Vite</span>
+              <span className={style.spanTecno}>Redux</span>
+              <span className={style.spanTecno}>Express</span>
+              <span className={style.spanTecno}>Node</span>
+              <span className={style.spanTecno}>MongoDB</span>
+            </div>
           </div>
         </div>
-        <div className={style.projects}>
-          <div className={style.titleGh}>
-            <a href="https://granodeoro.vercel.app/" target="_blanck">
-              <h4>Grano de Oro</h4>
+
+        <div>
+          <div className={style.card}>
+            <div className={style.cardimage}>
+              <img className={style.imgsize} src={granoDeOro} onClick={() => openModal(granoDeOro)} />
+            </div>
+            <a href="https://github.com/gonzalo00913/Dashboard" target="_blank">
+              <h4 className={style.cardtitle}>Grano de Oro</h4>
             </a>
-            <a
-              href="https://github.com/gonzalo00913/Dashboard"
-              target="_blanck"
-            >
-              <img className={style.iconsMediaSize} src={github} alt="GitHub" />
-            </a>
-          </div>
-          <p className={style.desProjects}>
-            Contribuí al desarrollo front-end de una pagina web dedicada a la
-            venta de café.
-          </p><br />
-          <div className={style.containerTecno}>
-            <span className={style.spanTecno}>JavaScript</span>
-            <span className={style.spanTecno}>Vite</span>
-            <span className={style.spanTecno}>Redux</span>
-            <span className={style.spanTecno}>Sequelize</span>
-            <span className={style.spanTecno}>Express</span>
-            <span className={style.spanTecno}>Node</span>
-            <span className={style.spanTecno}>PostgreSQL</span>
+            <p className={style.cardbody}>
+              Pagina web dedicada a la venta de cafe. Mi rol consistió en diseñar la función de prohibir o suspender usuarios de sitio web.
+            </p>
+            <div className={style.containerTecno}>
+              <span className={style.spanTecno}>JavaScript</span>
+              <span className={style.spanTecno}>Vite</span>
+              <span className={style.spanTecno}>Redux</span>
+              <span className={style.spanTecno}>Sequelize</span>
+              <span className={style.spanTecno}>Express</span>
+              <span className={style.spanTecno}>Node</span>
+              <span className={style.spanTecno}>PostgreSQL</span>
+            </div>
           </div>
         </div>
-        <div className={style.projects}>
-          <div className={style.titleGh}>
-            <a href="https://meddyplus.netlify.app/" target="_blanck">
-              <h4>Meddy plus</h4>
-            </a>
+
+        <div>
+          <div className={style.card}>
+            <div className={style.cardimage}>
+              <img className={style.imgsize} src={meddyPlus} onClick={() => openModal(meddyPlus)} />
+            </div>
             <a
               href="https://github.com/gonzalo00913/MeddyPlus-Gestion-de-Historiales-Medicos"
-              target="_blanck"
+              target="_blank"
             >
-              <img className={style.iconsMediaSize} src={github} alt="GitHub" />
+              <h4 className={style.cardtitle}>Meddy plus</h4>
             </a>
-          </div>
-          <p className={style.desProjects}>
-            Contribuí al desarrollo back-end de una pagina web dedicada a la
-            gestión de historiales clínicos.
-          </p>
-          <div className={style.containerTecno}>
-            <span className={style.spanTecno}>TypeScript</span>
-            <span className={style.spanTecno}>TypeORM</span>
-            <span className={style.spanTecno}>Vite</span>
-            <span className={style.spanTecno}>Node</span>
-            <span className={style.spanTecno}>PostgreSQL</span>
+            <p className={style.cardbody}>
+              Contribuí al desarrollo back-end de una pagina web dedicada a la
+              gestión de historiales clínicos.
+            </p>
+            <div className={style.containerTecno}>
+              <span className={style.spanTecno}>TypeScript</span>
+              <span className={style.spanTecno}>TypeORM</span>
+              <span className={style.spanTecno}>Vite</span>
+              <span className={style.spanTecno}>Node</span>
+              <span className={style.spanTecno}>PostgreSQL</span>
+            </div>
           </div>
         </div>
-        <div className={style.projects}>
-          <div className={style.titleGh}>
-            <a
-              href="https://food-app-gonzalo-masa.netlify.app/"
-              target="_blanck"
-            >
-              <h4>Food App</h4>
-            </a>
-            <a
-              href="https://github.com/gonzalo00913/Homemade-delights-Recetas-de-comida"
-              target="_blanck"
-            >
-              <img className={style.iconsMediaSize} src={github} alt="GitHub" />
-            </a>
-          </div>
-          <p className={style.desProjects}>
-            Permite buscar, filtrar y crear recetas, y utiliza Redux para la
-            gestión del estado de la aplicación
-          </p>
-          <div className={style.containerTecno}>
-            <span className={style.spanTecno}>JavaScript</span>
-            <span className={style.spanTecno}>React</span>
-            <span className={style.spanTecno}>Redux</span>
-            <span className={style.spanTecno}>Sequelize.js</span>
-            <span className={style.spanTecno}>Express.js</span>
-            <span className={style.spanTecno}>Node.js</span>
-            <span className={style.spanTecno}>PostgreSQL</span>
-          </div>
-        </div>
-        <div className={style.projects}>
-          <div className={style.titleGh}>
-            <a
-              href="https://gonzalo00913.github.io/Project--Three-Project-Calculator/"
-              target="_blanck"
-            >
-              <h4>Calculator</h4>
-            </a>
-            <a
-              href="https://github.com/gonzalo00913/Project--Three-Project-Calculator"
-              target="_blanck"
-            >
-              <img className={style.iconsMediaSize} src={github} alt="GitHub" />
-            </a>
-          </div>
-          <p className={style.desProjects}>
-            Utilicé JavaScript para manipular el DOM y crear la lógica de la
-            calculadora.
-          </p><br />
-          <div className={style.containerTecno}>
-            <span className={style.spanTecno}>JavaScript</span>
-            <span className={style.spanTecno}>CSS</span>
-            <span className={style.spanTecno}>Html</span>
-          </div>
-        </div>
-        <div className={style.projects}>
-          <div className={style.titleGh}>
-            <a
-              href="https://gonzalo00913.github.io/Project-Rock-Paper-Scissors/"
-              target="_blanck"
-            >
-              <h4>Rock-Paper-Scissors</h4>
-            </a>
-            <a
-              href="https://github.com/gonzalo00913/Project-Rock-Paper-Scissors"
-              target="_blanck"
-            >
-              <img className={style.iconsMediaSize} src={github} alt="GitHub" />
-            </a>
-          </div>
-          <p className={style.desProjects}>
-            En mis primeros pasos, realicé un proyecto utilizando JavaScript
-            Vanilla para manipular el DOM.
-          </p>
-          <div className={style.containerTecno}>
-            <span className={style.spanTecno}>JavaScript</span>
-            <span className={style.spanTecno}>CSS</span>
-            <span className={style.spanTecno}>Html</span>
-          </div>
-        </div>
-        <div className={style.projects}>
-          <div className={style.titleGh}>
-            <a
-              href="https://gonzalo00913.github.io/Etch-A-Sketch/"
-              target="_blanck"
-            >
-              <h4>Etch-A-Sketch</h4>
-            </a>
-            <a
-              href="https://github.com/gonzalo00913/Etch-A-Sketch"
-              target="_blanck"
-            >
-              <img className={style.iconsMediaSize} src={github} alt="GitHub" />
-            </a>
-          </div>
-          <p className={style.desProjects}>
-            Implementé eventos del mouse para capturar los movimientos del
-            cursor y dibujar líneas en el lienzo
-          </p>
-          <div className={style.containerTecno}>
-            <span className={style.spanTecno}>JavaScript</span>
-            <span className={style.spanTecno}>CSS</span>
-            <span className={style.spanTecno}>Html</span>
-          </div>
-        </div>
+
       </section>
+
+      <Modal isOpen={modalIsOpen} onRequestClose={closeModal} className={style.modal} overlayClassName={style.overlay}>
+        <button onClick={closeModal} className={style.closeButton}>X</button>
+        <img src={selectedImage} className={style.modalImage} />
+      </Modal>
     </div>
   );
 };
